9c115ec9ed08341091e8f07ba8a944812d64e305
JacoboGGLeon <jacoboggleon@gmail.com>
2026-02-25 17:13:03 -0600

    v101: react + tailwind + fastapi

--- agnostic_agent/api/server.py
+++ agnostic_agent/api/server.py
@@ -183,4 +183,4 @@ else:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("agnostic_agent.api.server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("agnostic_agent.api.server:app", host="0.0.0.0", port=8000, reload=True)
